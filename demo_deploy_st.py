e5a5af2933fbe00ad7311975e6967b53356d9927
elucidator040999 <elucidator0409@gmail.com>
2026-05-10 16:52:29 +0700

    Add files via upload

---
+++ demo_deploy_st.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Deploy Streamlit APP", text_alignment='center')
+df = pd.DataFrame([{'Ten': 'Phat','Tuoi': 18, 'Nghe': 'Bi'}])
+st.dataframe(df)
+if st.button(df):
+    st.balloons()
